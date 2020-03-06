474d7cc9b8de6e8a07904a51b04eeedb5246361b
Nicole Liu <nicoleliu_@outlook.com>
2020-03-05 21:17:52 -0500

    Playground to try out using averages and looping through arrays

---
+++ Arrays and averages.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+var numbers: [Int] = [5,6,10,2,17,28]
+print (numbers[0])
+print (numbers [3])
+
+print("-------------------------")
+var total = 0
+
+for number in numbers {
+    print(number)
+    total += number
+}
+
+print ("-----------------------")
+print (total)
+print (numbers.count)
+var average = total/numbers.count
+print(average)
